f7d21220499a8c2adcbfc008c3f8c003b8454dc7
hatem1055 <hatemmostafa31@gmail.com>
2021-01-06 15:22:32 +0200

    adding main item name as subitem in subitems screen

--- lib/widgets/general_item_widget.dart
+++ lib/widgets/general_item_widget.dart
@@ -1,43 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:myshwary_rewrite/providers/colors_provider.dart';
+import 'package:myshwary_rewrite/providers/item_provider.dart';
 import 'item_name.dart';
 import 'package:provider/provider.dart';
+
 class GeneralItemWidget extends StatelessWidget {
   final String name;
   final bool isDone;
   final String id;
   final Function deleteItem;
-  final Function toggleIsDone; 
+  final Function toggleIsDone;
   final String parentId;
-  GeneralItemWidget({
-    @required this.name,
-    @required this.isDone,
-    @required this.toggleIsDone,
-    @required this.deleteItem,
-    @required this.id,
-    this.parentId
-  });
+  GeneralItemWidget(
+      {@required this.name,
+      @required this.isDone,
+      @required this.toggleIsDone,
+      @required this.deleteItem,
+      @required this.id,
+      this.parentId});
   @override
   Widget build(BuildContext context) {
     final colors = Provider.of<ColorsProvider>(context);
+    final item = Provider.of<ItemProvider>(context);
     return ListTile(
       leading: Container(
-        width: MediaQuery.of(context).size.width * 0.9,
-        child: Row(children: [
-          Checkbox(
-            value: isDone,
-            materialTapTargetSize: MaterialTapTargetSize.padded,
-            activeColor: colors.colorPallet.bgColor.withOpacity(.8),
-            onChanged: (val) {
-              toggleIsDone(id:id, val:val,parentId:parentId);
-            },
-          ),
-          ItemName(name, isDone)
-        ]),
+        child: Checkbox(
+          value: isDone,
+          materialTapTargetSize: MaterialTapTargetSize.padded,
+          activeColor: colors.colorPallet.bgColor.withOpacity(.8),
+          onChanged: (val) {
+            toggleIsDone(id: id, val: val, parentId: parentId);
+          },
+        ),
       ),
+      title: ItemName(name, isDone),
+      subtitle: parentId == null ? null : Text(item.name),
       trailing: IconButton(
         onPressed: () {
-          deleteItem(parentId:parentId,id:id);
+          deleteItem(parentId: parentId, id: id);
         },
         icon: Icon(Icons.delete, color: Colors.red),
       ),
